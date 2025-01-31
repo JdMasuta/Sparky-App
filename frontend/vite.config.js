6f7af21f0645f51a233098ddf29346c571b6d2da
JdMasuta <155690173+JdMasuta@users.noreply.github.com>
2025-01-30 20:17:47 -0700

    Add files via upload
    finished uploading frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "../backend/src/public", // Build directly to backend's public folder
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
