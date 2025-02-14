04da739c0a2217af669b8c6b8eee9e359ceadffc
Josh Meesey <jdmeesey@gmail.com>
2025-02-14 06:20:12 -0700

    Use streamlined api endpoint

--- frontend/src/components/report/useFetchPullsData.jsx
+++ frontend/src/components/report/useFetchPullsData.jsx
@@ -6,37 +6,19 @@ const FetchPullsData = ({ setTodaysPulls, setWeeksPulls }) => {
 
   useEffect(() => {
     const fetchData = createDebounce(async (signal) => {
-      const today = new Date();
-      const midnightToday = new Date(today.setHours(0, 0, 0, 0))
-        .toISOString()
-        .slice(0, 19)
-        .replace("T", " ");
-      const midnightWeekAgo = new Date(today.setDate(today.getDate() - 7))
-        .toISOString()
-        .slice(0, 19)
-        .replace("T", " ");
-
-      const [todaysData, weeksData] = await Promise.all([
-        fetch("/api/checkouts/detailed/after", {
-          method: "POST",
+      const result = await Promise.all([
+        fetch("/api/checkouts/stats", {
+          method: "GET",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ timestamp: midnightToday }),
-          signal,
-        }).then((res) => res.json()),
-        fetch("/api/checkouts/detailed/after", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ timestamp: midnightWeekAgo }),
           signal,
         }).then((res) => res.json()),
       ]);
+      console.log(result);
 
-      setTodaysPulls(todaysData.length);
-      setWeeksPulls(weeksData.length);
+      setTodaysPulls(result[0].today_count);
+      setWeeksPulls(result[0].week_count);
     }, 1000);
 
     // Store the debounced function in a ref so we can clean it up
