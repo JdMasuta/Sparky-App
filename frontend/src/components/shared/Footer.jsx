0b1883c0453f2c597894bb9df2e74d07bc1bbfbe
Josh Meesey <jdmeesey@gmail.com>
2025-04-10 10:56:19 -0600

    Versioning update 1.0.2

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <footer className="app-footer">
       <div>Sparky Cart Project 2025</div>
-      <div>Contributors: Josh Meesey, Jeremiah Holabird, Robert Smith</div>
-      <div>Version 1.0.1</div>
+      <div>✨Contributors: Josh Meesey, Jeremiah Holabird, Robert Smith✨</div>
+      <div>Version 1.0.2</div>
     </footer>
   );
 };
